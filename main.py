0744a8c8fae913c8d4ee5be2d7fc71e2215b1fe4
phazify <101293535+phazify@users.noreply.github.com>
2022-03-10 19:31:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
-import discord
-import colorama
+import requests
 import os
+import sys
+import threading
+import time
+import json
+import asyncio
+import discord
+import aiohttp
+import pprint
+
 from colorama import Fore
-def startprint():
+from pypresence import Presence
+from discord import Webhook, AsyncWebhookAdapter
+from discord.ext import commands
 
-    print(f'''{Fore.RESET}
+os.system(f'cls & mode 85,20 & title PHAZIFY MASS DM')
 
-            ╔═╗╦ ╦╔═╗╔═╗╦╔═╗╦ ╦
-            ╠═╝╠═╣╠═╣╔═╝║╠╣ ╚╦╝
-            ╩  ╩ ╩╩ ╩╚═╝╩╚   ╩ 
-
-    ''' + Fore.RESET)
-
-
-def Clear():
-    os.system('cls')
+print(f'''{Fore.RESET}
+                  
+                   ╔═╗╦ ╦╔═╗╔═╗╦╔═╗╦ ╦
+                   ╠═╝╠═╣╠═╣╔═╝║╠╣ ╚╦╝
+                   ╩  ╩ ╩╩ ╩╚═╝╩╚   ╩ 
+        
+        ''' + Fore.RESET) 
 
 
 client = discord.Client()
+
+token = input("Type in your token retard")
+sex = input("Msg?")
 
 @client.event
 async def on_connect():
   for user in client.user.friends:
     try:
-      await user.send('message to send here')
+      await user.send(sex)
       print(f"messaged: {user.name}")
     except:
        print(f"couldnt message: {user.name}")
 
-client.run('token here', bot=False)
+client.run(token, bot=False)
